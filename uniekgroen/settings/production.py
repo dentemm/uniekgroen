75315ba0a2e73215f415737f96c04549ea88df01
Tim Claes <tim.claes@me.com>
2019-11-01 14:10:41 +0100

    setting up heroku

--- uniekgroen/settings/production.py
+++ uniekgroen/settings/production.py
@@ -6,3 +6,38 @@ try:
     from .local import *
 except ImportError:
     pass
+
+
+#
+# GENERAL SETTINGS
+#
+
+WSGI_APPLICATION = 'healthhouse.wsgi.application'
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.uniekgroen.be', '.aws.amazon.com', '.herokuapp.com']
+
+# Enforce SSL
+SECURE_SSL_REDIRECT = True
+
+#
+# MIDDLEWARE
+#
+
+MIDDLEWARE += [
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+]
+
+#
+# DATABASE
+#
+
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+#
+# STATIC FILES
+#
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
