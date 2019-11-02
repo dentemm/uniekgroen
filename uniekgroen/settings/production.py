7ae517f64b25b84cf8fd4855b4d95a7ac20a9d0d
Tim Claes <tim.claes@me.com>
2019-11-02 09:19:41 +0100

    amazon s3 setup

--- uniekgroen/settings/production.py
+++ uniekgroen/settings/production.py
@@ -37,3 +37,12 @@ DATABASES['default'] =  dj_database_url.config()
 # AMAZON AWZS
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_STORAGE_BUCKET_NAME = 'uniekgroen'
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.eu-central-1.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+
+MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
