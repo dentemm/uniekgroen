361908289d1774697dd69217ba486dfff16f292e
Tim Claes <tim.claes@me.com>
2020-01-09 15:43:53 +0100

    upgrading navigation area

--- uniekgroen/static/js/uniekgroen.js
+++ uniekgroen/static/js/uniekgroen.js
@@ -1,9 +1,22 @@
-var distance = $('.temm').offset().top;
+window.onload = function () {
 
-$(window).scroll(function() {
-    if ( $(this).scrollTop() >= distance ) {
-        console.log('is in top');
-    } else {
-        console.log('is not in top');
-    }
-});
+    let distance = $('.sticky-nav').offset().top;
+
+    $(window).scroll(function() {
+
+        let el = $('.sticky-nav>div').first();
+        let titles = $('.sticky-nav').find('div.description');
+        let icons = $('.sticky-nav').find('div.icon');
+
+        if ( $(this).scrollTop() >= distance ) {
+            el.removeClass('py-5');
+            titles.attr('hidden', true);
+            icons.addClass('icon-sm');
+        } else if (!el.hasClass('py-5')) {
+            el.addClass('py-5');
+            titles.attr('hidden', false);
+            icons.removeClass('icon-sm');
+        }
+    });
+};
+
