53ff51c76056799df1c4e46492fa413a9c4621ff
Tim Claes <tim.claes@me.com>
2020-01-10 11:10:57 +0100

    improvements to navbar script

--- uniekgroen/static/js/uniekgroen.js
+++ uniekgroen/static/js/uniekgroen.js
@@ -1,22 +1,33 @@
 window.onload = function () {
 
     let distance = $('.sticky-nav').offset().top;
+    let canMove = true;
+    // const isChrome = !!window.chrome;
 
-    $(window).scroll(function() {
-
+    const changeNavBar = () => {
         let el = $('.sticky-nav>div').first();
         let titles = $('.sticky-nav').find('div.description');
         let icons = $('.sticky-nav').find('div.icon');
 
-        if ( $(this).scrollTop() >= distance ) {
+        if ( $(this).scrollTop() >= distance) {
             el.removeClass('py-5');
             titles.attr('hidden', true);
             icons.addClass('icon-sm');
+
+            if (canMove) {
+                $(this).scrollTop(distance);
+                canMove = false;
+            }
+
         } else if (!el.hasClass('py-5')) {
             el.addClass('py-5');
             titles.attr('hidden', false);
             icons.removeClass('icon-sm');
-        }
-    });
+
+            canMove = true;
+        } 
+    };
+
+    $(window).scroll(changeNavBar);
 };
 
